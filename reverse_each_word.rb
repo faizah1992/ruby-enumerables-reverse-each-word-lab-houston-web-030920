4f75f38c570542c5d45c9182a15bdc0775d89b10
FAIZAH AHSAN <faizah1992@gmail.com>
2020-03-02 19:07:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,12 @@
 def reverse_each_word (string)
  split_str=string.split(" ")
  reversed=[]
-
+ i=0 
+ while i<split_str do 
+   item= split_str[i]
+   result= item.reverse()
+   reverse.push(result)
+   i += 1 
+end
+reversed
 end
